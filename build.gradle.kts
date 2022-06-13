d0745bdb1803a5092bccabab965df01e05c3f4e1
mooziii <63669478+mooziii@users.noreply.github.com>
2022-06-13 15:32:42 +0200

    Curseforge publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,7 @@
+import com.matthewprenger.cursegradle.CurseProject
+import com.matthewprenger.cursegradle.CurseRelation
+import com.matthewprenger.cursegradle.Options
+
 plugins {
     kotlin("jvm") version "1.7.0"
     kotlin("plugin.serialization") version "1.7.0"
@@ -5,6 +9,7 @@ plugins {
     id("io.github.juuxel.loom-quiltflower") version "1.7.2"
     id("org.quiltmc.quilt-mappings-on-loom") version "4.2.0"
     id("com.modrinth.minotaur") version "2.+"
+    id("com.matthewprenger.cursegradle") version "1.4.0"
     java
 }
 
@@ -63,6 +68,12 @@ tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "$javaVersion"
     }
+    named("curseforge") {
+        onlyIf {
+            System.getenv("CURSEFORGE_TOKEN") != null
+        }
+        dependsOn(remapJar)
+    }
 }
 
 modrinth {
@@ -74,10 +85,33 @@ modrinth {
     loaders.add("fabric")
     loaders.add("quilt")
     dependencies {
-        required.project("Ha28R6CL")
-        optional.project("mOgUt4GM")
-        required.project("9s6osm5g")
+        required.project("Ha28R6CL") // fabric language kotlin
+        required.project("9s6osm5g") // cloth config
+        optional.project("mOgUt4GM") // mod menu
     }
 
     uploadFile.set(tasks.remapJar.get())
+}
+
+curseforge {
+    project(closureOf<CurseProject> {
+        apiKey = System.getenv("CURSEFORGE_TOKEN")
+        mainArtifact(tasks.remapJar.get())
+
+        id = "632589"
+        releaseType = "release"
+        addGameVersion("${project.version.toString().split("mc")[1]}")
+        addGameVersion("Java 17")
+        addGameVersion("Fabric")
+        addGameVersion("Quilt")
+
+        relations(closureOf<CurseRelation> {
+            requiredDependency("cloth-config")
+            requiredDependency("fabric-language-kotlin")
+            optionalDependency("modmenu")
+        })
+    })
+    options(closureOf<Options> {
+        forgeGradleIntegration = false
+    })
 }
